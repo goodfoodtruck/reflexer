9ca0cf2630308bdcaa453930e880029cdecc6df7
Louis Driffort <louis.driffort@gmail.com>
2026-05-21 00:24:34 +0200

    feat: execute entity triggered passives

--- workspaces/packages/engine/src/fight/turn-executors/EntityPassiveExecutor.ts
+++ workspaces/packages/engine/src/fight/turn-executors/EntityPassiveExecutor.ts
@@ -1,16 +1,44 @@
-import { ActionLog, PlayingEntity } from "@fight/fight.types";
+import { ActionLog, ExecutionContext, PlayingEntity } from "@fight/fight.types";
 import { FightContext } from "@fight/context/FightContext";
-import { PassiveTrigger } from "@fight/passives/passives.types";
+import { Passive, PassiveTrigger, TriggeredPassive } from "@fight/passives/passives.types";
+import { GambitTargetResolver } from "@fight/gambits/resolvers/target/GambitTargetResolver";
+import { EntityActionExecutor } from "@fight/turn-executors/EntityActionExecutor";
+
+const isTriggeredPassiveOfType = (triggerType: PassiveTrigger) => {
+        return (passive: Passive): passive is TriggeredPassive =>
+        passive.kind === "TRIGGERED" && passive.triggerType === triggerType
+}
 
 export class EntityPassiveExecutor {
-    
-    constructor() {}
+
+    constructor(
+        private readonly targetResolver: GambitTargetResolver,
+        private readonly actionExecutor: EntityActionExecutor
+    ) {}
 
     executePassiveTrigger(
-        trigger: PassiveTrigger,
+        triggerType: PassiveTrigger,
         entity: PlayingEntity, 
         fightContext: FightContext
     ): ActionLog[] {
-        return []
+        const logs: ActionLog[] = []
+
+        const passives = entity.activePassives.map(ap => ap.passive).filter(isTriggeredPassiveOfType(triggerType))
+
+        for (const passive of passives) {
+            const actionTargetId = this.targetResolver.resolve(entity, fightContext, passive.targetSelector)
+            if (! actionTargetId) continue
+
+            const executionContext: ExecutionContext = {
+                casterId: entity.id,
+                actionId: passive.triggeredActionId,
+                targetId: actionTargetId,
+                reactionDepth: 0
+            }
+
+            logs.push(...this.actionExecutor.execute(executionContext, fightContext))
+        }
+        
+        return logs
     }
 }
