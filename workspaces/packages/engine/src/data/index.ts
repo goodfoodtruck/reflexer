98e181b9f489fee391aefb5e49bd362a2ec66ea1
Celina CHABANE <celina.chabane@acensi.fr>
2026-05-27 21:32:27 +0200

    Implementation Registry

--- workspaces/packages/engine/src/data/index.ts
+++ workspaces/packages/engine/src/data/index.ts
@@ -2,3 +2,6 @@ export * from "./IActionRegistry";
 export * from "./IEnemyRegistry";
 export * from "./IFightMapRegistry";
 export * from "./InMemoryActionRegistry";
+export * from "./InMemoryAllyRegistry";
+export * from "./InMemoryEnemyRegistry";
+export * from "./InMemoryFightMapRegistry";
