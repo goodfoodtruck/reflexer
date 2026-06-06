f3fd11d888e834ce411cec29f644bfb1e06f7a10
Louis Driffort <louis.driffort@gmail.com>
2026-06-06 14:54:24 +0200

    move server folder in new apis workspace, add tsup for build

--- workspaces/packages/engine/src/data/index.ts
+++ workspaces/packages/engine/src/data/index.ts
@@ -2,6 +2,5 @@ export * from "./IActionRegistry";
 export * from "./IEnemyRegistry";
 export * from "./IFightMapRegistry";
 export * from "./InMemoryActionRegistry";
-export * from "./InMemoryAllyRegistry";
 export * from "./InMemoryEnemyRegistry";
 export * from "./InMemoryFightMapRegistry";
