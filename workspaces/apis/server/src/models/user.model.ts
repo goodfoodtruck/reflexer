0f4fb51a9cdc76e50431140bb2b6e5beecbcd38b
Celina CHABANE <celina.chabane@acensi.fr>
2026-06-07 18:38:29 +0200

    change model & route on sever api

--- workspaces/apis/server/src/models/user.model.ts
+++ workspaces/apis/server/src/models/user.model.ts
@@ -1,14 +1,18 @@
 import { Schema, model, type Document } from "mongoose"
  
 export interface UserDocument extends Document {
-    name:      string
+    name: string
+    password: string
+    secretAnswer: string
     createdAt: Date
     updatedAt: Date
 }
- 
+
 const UserSchema = new Schema<UserDocument>(
     {
-        name: { type: String, required: true }
+        name: { type: String, required: true, unique: true, trim: true },
+        password: { type: String, required: true },
+        secretAnswer: { type: String, required: true },
     },
     { timestamps: true }
 )
