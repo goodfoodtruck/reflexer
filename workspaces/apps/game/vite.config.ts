3e3b9cc0d8e99d156ab33f8864c6a1251b475486
Louis Driffort <louis.driffort@gmail.com>
2026-06-09 11:04:22 +0200

    add aliases

--- workspaces/apps/game/vite.config.ts
+++ workspaces/apps/game/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@pages":      path.resolve(__dirname, "./src/pages"),
+            "@hooks":      path.resolve(__dirname, "./src/hooks"),
+            "@services":   path.resolve(__dirname, "./src/services"),
+            "@assets":     path.resolve(__dirname, "./src/assets")
+        }
+    }
 })
