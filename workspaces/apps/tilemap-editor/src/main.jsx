129d0e2599dd19704d31aa6dffe9b1464b789f1b
goodfoodtruck <mielatomique@protonmail.com>
2026-06-19 00:14:13 +0200

    #271 add tilemap-editor to project apps

---
+++ workspaces/apps/tilemap-editor/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TilemapEditor from "./TilemapEditor.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TilemapEditor />
+  </React.StrictMode>
+);
